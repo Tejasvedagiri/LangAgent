81bf9f6b2c6df0d0a13b6640051a2d94dd3cfa53
Tejasvedagiri <tejasvedagiri1223@gmail.com>
2025-03-22 20:49:59 -0500

    Update main.py

--- src/lang_agent/main.py
+++ src/lang_agent/main.py
@@ -1,12 +1,12 @@
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
-from dotenv import load_dotenv
-import os
+from langchain_core.output_parsers import StrOutputParser
 
 if __name__=="__main__":
     llm = ChatOpenAI(
         base_url="http://192.168.0.111:1234/v1",
         model="deepseek-r1-distill-llama-8b-abliterated",
+        max_tokens=250,
         api_key="dummy")
     summary_template = '''
     Given a {object} describe 
@@ -15,7 +15,7 @@ if __name__=="__main__":
     '''
     summary_prompt_template = PromptTemplate(input_variables=["object"], template=summary_template)
 
-    chain = summary_prompt_template | llm
+    chain = summary_prompt_template | llm | StrOutputParser()
 
     res = chain.invoke(input={
         "object": "table"
